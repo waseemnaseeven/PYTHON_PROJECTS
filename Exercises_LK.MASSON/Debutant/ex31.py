fd6782a2e133c4a2227c60b9eac22e4d249c66ba
Waseem NASEEVEN <wnaseeve@e1r6p10.clusters.42paris.fr>
2023-05-05 12:21:53 +0200

    new folders, pushing exercises, getting a little bit more complicated (we are now using functions)

---
+++ Exercises_LK.MASSON/Debutant/ex31.py
@@ -0,0 +1,7 @@
+for i in range(8):
+    for j in range(5, 21):
+        if j == 20:
+        	print(j)
+        else:
+            print(j, end=" ")
+
---
+++ Exercises_LK.MASSON/Debutant/ex32.py
@@ -0,0 +1,3 @@
+L = [i for i in range(3,25) if i % 3 == 0]
+L1 = [x / 3 for x in L]
+print(L1)
